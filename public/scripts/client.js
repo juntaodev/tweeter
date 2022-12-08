06d3efa93c103306c9e609f753c06bda9a79e03f
juntaodev <juntaoliudev@gmail.com>
2022-12-08 00:27:36 -0500

    Add dynamic tweets to tweet container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,34 +9,47 @@ $(document).ready(function () {
 
 
   // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  };
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
 
-  const createTweetElement = function(tweetData) {
-    console.log(tweetData.content.text);
+  const createTweetElement = function(data) {
+    console.log(data.content.text);
     const tweet = $(`
     <div class="tweet-container">
       <div class="tweet-user">
         <div class="tweet-user-name">
-          <img class="image" src=${tweetData.user.avatars}></img>
-          <p class="username">${tweetData.user.name}</p>
+          <img class="image" src=${data.user.avatars}></img>
+          <p class="username">${data.user.name}</p>
         </div>
         <div class="tweet-user-id">
-          <p>${tweetData.user.handle}</p>
+          <p>${data.user.handle}</p>
         </div>
       </div>
-      <article class="tweet">${tweetData.content.text}</article>
+      <article class="tweet">${data.content.text}</article>
       <footer>
-        <div>${tweetData.created_at}</div>
+        <div>${data.created_at}</div>
         <div class="small-icons">
           <a><i class="fa-solid fa-flag"></i></a>
           <a><i class="fa-solid fa-repeat"></i></a>
@@ -48,12 +61,19 @@ $(document).ready(function () {
     return tweet;
   };
 
-  const $tweet = createTweetElement(tweetData);
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // get return value and append it to tweets container
+    tweets.forEach(element => {
+      const $tweet = createTweetElement(element);
+      $("#tweets-container").append($tweet[0]);
+    });
+  };
+
+  renderTweets(data);
 
   
 
-  // Test / driver code (temporary)
-  console.log($tweet[0]); // to see what it looks like
-  $("#tweets-container").append($tweet[0]); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
 });
